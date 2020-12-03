7921db441d638eed8da772b26fb46aeafc0b3a85
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2020-12-02 22:01:44 -0300

    ajustes

---
+++ App.tsx
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  const [camera, setCamera] = useState(null);
+
+  const takePicture = async () => {
+    if (camera) {
+      const options = {quality: 0.5, base64: true};
+      const data = await camera.takePictureAsync(options);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <RNCamera
+        ref={(ref) => {
+          setCamera(ref);
+        }}
+        style={styles.preview}
+        type={RNCamera.Constants.Type.front}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'We need your permission to use your camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+        androidRecordAudioPermissionOptions={{
+          title: 'Permission to use audio recording',
+          message: 'We need your permission to use your audio',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancel',
+        }}
+      />
+      <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+        <TouchableOpacity
+          onPress={takePicture.bind(this)}
+          style={styles.capture}>
+          <Text style={{fontSize: 14}}> Capturar </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
+
+export default App;
