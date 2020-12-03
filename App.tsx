bcf2b4ca8b2bb8b296f303409a6b6d5bb668c764
André Felipe <andrefeliperf17@gmail.com>
2020-12-03 15:52:18 -0300

    ajustes

--- App.tsx
+++ App.tsx
@@ -5,43 +5,46 @@ import {RNCamera} from 'react-native-camera';
 declare const global: {HermesInternal: null | {}};
 
 const App = () => {
-  const [camera, setCamera] = useState(null);
+  const [camera, setCamera] = useState<any>();
 
   const takePicture = async () => {
     if (camera) {
       const options = {quality: 0.5, base64: true};
-      const data = await camera.takePictureAsync(options);
+      const response = await camera.takePictureAsync(options);
+      console.log(response.uri);
     }
   };
 
   return (
     <View style={styles.container}>
-      <RNCamera
-        ref={(ref) => {
-          setCamera(ref);
-        }}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.front}
-        flashMode={RNCamera.Constants.FlashMode.on}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        androidRecordAudioPermissionOptions={{
-          title: 'Permission to use audio recording',
-          message: 'We need your permission to use your audio',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-      />
-      <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
-        <TouchableOpacity
-          onPress={takePicture.bind(this)}
-          style={styles.capture}>
-          <Text style={{fontSize: 14}}> Capturar </Text>
-        </TouchableOpacity>
+      <View style={styles.content}>
+        <RNCamera
+          ref={(ref) => {
+            setCamera(ref);
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.front}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+        />
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+          <TouchableOpacity
+            onPress={() => takePicture()}
+            style={styles.capture}>
+            <Text style={{fontSize: 14}}> Capturar </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -50,8 +53,11 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
   },
   preview: {
     flex: 1,
@@ -60,12 +66,10 @@ const styles = StyleSheet.create({
   },
   capture: {
     flex: 0,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    color: '#fff',
     borderRadius: 5,
     padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
   },
 });
 
